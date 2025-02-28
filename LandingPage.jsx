608d67c24753e8f5e1a803457f05c44eae9168a1
Rathod Ravinder <149700674+rathod1234567@users.noreply.github.com>
2025-02-28 11:16:35 +0530

    Add files via upload

---
+++ LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import "./LandingPage.css"
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <h1>CricPulse</h1>
+        <div className='img1'>
+          <img src="https://fancricpulse.com/img/Default_Welcome_to_Runcase_Cricket_mobile_app_screen_1.jpg" alt="img"  width={400} height={400}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
